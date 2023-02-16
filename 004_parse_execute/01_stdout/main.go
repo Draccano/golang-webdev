838fa9faccee551502266e9fa241c6fb6b6ec3f9
Adam Richter <draccanos@gmail.com>
2023-02-16 22:37:40 +0100

    work with text/template

---
+++ 004_parse_execute/01_stdout/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// Do not use the above code in production, better approaches soon.
